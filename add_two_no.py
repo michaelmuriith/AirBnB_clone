594b43161e635d4b1cce1784ff2991d8d14571a4
michaelmuriith <mikemuriith@gmail.com>
2022-11-22 17:43:22 +0300

    structuring

---
+++ add_two_no.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''code to compare two no'''
+
+def compare_two_no(x, y):
+    if x > y:
+        print("x is greater")
+    else:
+        print("y is greater")
+
+'''call function'''
+compare_two_no(5, 10)
