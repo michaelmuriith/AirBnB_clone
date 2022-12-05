8b2f979c92c51e59d4e1d634d963e323aed161ca
michaelmuriith <mikemuriith@gmail.com>
2022-12-05 20:50:11 +0300

    init

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,7 @@
 The __init__ dunder method for the models
 Makes the models directory become a package
 """
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
